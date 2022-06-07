e5dc687d24459f2d15c671234e6cfb9704f5ac03
Ansh <ansh1999arora@gmail.com>
2022-06-07 21:46:40 +0200

    Fix layout shifting of the hero image with HTML/CSS

--- src/scenes/HomePage/components/Hero.tsx
+++ src/scenes/HomePage/components/Hero.tsx
@@ -1,9 +1,9 @@
-import s from "./Hero.module.css";
+import s from './Hero.module.css';
 
 export const Hero = () => {
   return (
     <section className={s.container}>
-      <img className={s.image} src="bg.jpg" alt="" />
+      <img className={s.image} height="800" width="800" src="bg.jpg" alt="" />
       <div className={s.center}>
         <h1 className={s.title}>Star fighter</h1>
         <a className={s.button} href="/game">
